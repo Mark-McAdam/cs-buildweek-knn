bc13dd7430d08a916fff3d8b38d28bec7fb1fbb5
Mark <mark.a.mcadam@gmail.com>
2020-07-29 13:43:55 -0700

    uploads csv iterated knn

--- knn.py
+++ knn.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 import pandas as pd
+import warnings
 
+from collections import Counter
+from matplotlib import style
+from math import sqrt
 
-# The magic distance formulae 
+# The magic distance formulae
 # euclidean distance = sqrt( (x1-x2)**2 + (y1-y2)**2 )
 
 # two mock classes and their features
-dataset = {'k':[[1,2], [2,3], [3,1]], 'r':[[6,5], [7,7], [8,6]]}
-new_features = [5,7]
+dataset = {"k": [[1, 2], [2, 3], [3, 1]], "r": [[6, 5], [7, 7], [8, 6]]}
+new_features = [5, 7]
 
 # for i in dataset:
 #   for ii in dataset[i]:
 #     plt.scatter(ii[0], ii[1], s=100, color=i)
 
-# in one line now for refactor 
-[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset];
-plt.scatter(new_features[0], new_features[1], s=100, color='yellow')
+# in one line now for refactor
+[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+plt.scatter(new_features[0], new_features[1], s=100, color="yellow")
 plt.show()
--- knn_sklearn.py
+++ knn_sklearn.py
@@ -1,6 +1,6 @@
 from sklearn import neighbors
 
 knn = neighbors.KNeighborsClassifier(n_neighbors=10, n_jobs=-1)
-train['cluster'] = knn.fit(train[['longitude']], train[['latitude']])
-test['cluster'] = knn.predict(test[['longitude', 'latitude']])
-px.scatter(train, x='longitude', y='latitude', color='cluster')
+# train['cluster'] = knn.fit(train[['longitude']], train[['latitude']])
+# test['cluster'] = knn.predict(test[['longitude', 'latitude']])
+# px.scatter(train, x='longitude', y='latitude', color='cluster')
